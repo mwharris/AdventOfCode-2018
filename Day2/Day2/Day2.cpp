9bcf2c8789a81918fdb232eac16bd8458a00a2a0
Michael Harris <mharris@lbisoftware.com>
2018-12-05 19:00:29 -0500

    Setup for Day 2

---
+++ Day2/Day2/Day2.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    // Declare int counts for strings with exactly 2 or 3 of any letter
+
+	// Build an array/list/vector/map of ints, one for each letter in the alphabet
+
+	// LOOP for every line in the input file
+		// LOOP for every character in the string
+			// Process each character one-by-one and increase the associated int counter
+			// When done processing this line, if any letters are 2 or 3, increase counts accordingly
+		// END LOOP
+	// END LOOP
+}
